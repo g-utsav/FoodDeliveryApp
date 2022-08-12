e97af59037ca673029443fbc76fdb3bedf0be904
g-utsav <gutsav9@gmail.com>
2022-08-12 10:53:33 +0530

    File Structure

---
+++ foodDeliveryApp/src/main/java/com/foodDelivery/FoodDeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.foodDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryAppApplication.class, args);
+	}
+
+}
