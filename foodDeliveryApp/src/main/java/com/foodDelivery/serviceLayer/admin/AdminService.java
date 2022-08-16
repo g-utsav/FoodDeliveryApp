722359bd918f8582c204ba8e858233537e85a34d
g-utsav <gutsav9@gmail.com>
2022-08-16 17:09:12 +0530

    user varification

--- foodDeliveryApp/src/main/java/com/foodDelivery/serviceLayer/admin/AdminService.java
+++ foodDeliveryApp/src/main/java/com/foodDelivery/serviceLayer/admin/AdminService.java
@@ -2,9 +2,10 @@ package com.foodDelivery.serviceLayer.admin;
 
 import com.foodDelivery.entity.dto.AdminDto;
 import com.foodDelivery.entity.dto.CustomerToken;
-import com.foodDelivery.entity.dto.RestaurantDto;
 import com.foodDelivery.exceptions.AdminAcessNotGrantedException;
+import com.foodDelivery.exceptions.UserAccessNotGrantedException;
 import com.foodDelivery.exceptions.UserNotFound;
+import com.foodDelivery.exceptions.UserNotLoggedInException;
 
 public interface AdminService {
 
@@ -13,5 +14,7 @@ public interface AdminService {
 	public AdminDto removeAdmin(String token, String mToken, Integer custId)throws AdminAcessNotGrantedException,UserNotFound;
 	
 	public boolean verifyAdmin(CustomerToken cToken,Integer custId) throws AdminAcessNotGrantedException ;
+	
+	public boolean verifyUser(CustomerToken cToken) throws UserAccessNotGrantedException,UserNotFound,UserNotLoggedInException;
 	
 }
