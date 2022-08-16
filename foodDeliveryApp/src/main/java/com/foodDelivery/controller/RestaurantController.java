f2c6e61fcc6da12789b9500e3146388fbf8ecc57
g-utsav <gutsav9@gmail.com>
2022-08-17 00:55:44 +0530

    uri change

--- foodDeliveryApp/src/main/java/com/foodDelivery/controller/RestaurantController.java
+++ foodDeliveryApp/src/main/java/com/foodDelivery/controller/RestaurantController.java
@@ -29,7 +29,7 @@ import com.foodDelivery.serviceLayer.ResturantService;
 import com.foodDelivery.serviceLayer.admin.AdminService;
 
 @RestController
-@RequestMapping("/Restaurant")
+@RequestMapping("/restaurant")
 public class RestaurantController {
 
 	@Autowired
