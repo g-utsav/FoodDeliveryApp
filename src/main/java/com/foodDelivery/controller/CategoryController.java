3533c84b81d00d55016f1641206df234986e4e63
g-utsav <gutsav9@gmail.com>
2022-09-24 22:02:28 +0530

    Fix : CategoryController Import Bug Fix-1

--- src/main/java/com/foodDelivery/controller/CategoryController.java
+++ src/main/java/com/foodDelivery/controller/CategoryController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -57,7 +60,7 @@ public class CategoryController {
 		
 	}
 
-    @GetMapping (value = "/")
+    @GetMapping(value = "/")
     public ResponseEntity<String> generalMessage (){
         return new ResponseEntity<>("This is Category Controller visit https://github.com/g-utsav/FoodDeliveryApp for URL extension Detail.",HttpStatus.OK);
     }
--- src/main/java/com/foodDelivery/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/foodDelivery/exceptions/GlobalExceptionHandler.java
@@ -1,68 +1,68 @@
-package com.foodDelivery.exceptions;
-
-import java.time.LocalDateTime;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-@ControllerAdvice
-
-public class GlobalExceptionHandler {
-	
-	
-	//to handle specific UserNotFoundException
-	
-	/*@ExceptionHandler(UserNotFound.class)
-	public ResponseEntity<MyErrorDetails> myIllegalHandler(UserNotFound ie,WebRequest req) {
-	
-	MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), req.getDescription(false));
-	ResponseEntity<MyErrorDetails> re=new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
-	return re;
-	}
-	
-	
-	
-	//to handle specific NullValueerrorException
-	
-	@ExceptionHandler(NullValueerror.class)
-	public ResponseEntity<MyErrorDetails> myIllegalHandler(NullValueerror ie,WebRequest req) {
-	System.out.println("inside myHandler method...");
-	MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), req.getDescription(false));
-	ResponseEntity<MyErrorDetails> re=new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
-	return re;
-	}
-	
-	
-	
-	//to handle specific InvalidURIException
-	
-		@ExceptionHandler(InvalidURIException.class)
-		public ResponseEntity<MyErrorDetails> myIllegalHandler(InvalidURIException ie,WebRequest req) {
-		System.out.println("inside myHandler method...");
-		MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), req.getDescription(false));
-		ResponseEntity<MyErrorDetails> re=new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
-		return re;
-		}
-
-	
-	
-	
-		@ExceptionHandler(Exception.class)
-		public ResponseEntity<MyErrorDetails> myExceptionHandler(Exception e,WebRequest req) {
-		MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
-		return new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
-		}
-
-	
-	@ExceptionHandler(NoHandlerFoundException.class)
-	public ResponseEntity<MyErrorDetails> mynotFoundHandler(NoHandlerFoundException nfe,WebRequest req) {
-	MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), nfe.getMessage(), req.getDescription(false));
-	return new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
-	}*/
-	
-
-}
+//package com.foodDelivery.exceptions;
+//
+//import java.time.LocalDateTime;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.servlet.NoHandlerFoundException;
+//
+//@ControllerAdvice
+//
+//public class GlobalExceptionHandler {
+//	
+//	
+//	//to handle specific UserNotFoundException
+//	
+//	/*@ExceptionHandler(UserNotFound.class)
+//	public ResponseEntity<MyErrorDetails> myIllegalHandler(UserNotFound ie,WebRequest req) {
+//	
+//	MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), req.getDescription(false));
+//	ResponseEntity<MyErrorDetails> re=new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+//	return re;
+//	}
+//	
+//	
+//	
+//	//to handle specific NullValueerrorException
+//	
+//	@ExceptionHandler(NullValueerror.class)
+//	public ResponseEntity<MyErrorDetails> myIllegalHandler(NullValueerror ie,WebRequest req) {
+//	System.out.println("inside myHandler method...");
+//	MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), req.getDescription(false));
+//	ResponseEntity<MyErrorDetails> re=new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+//	return re;
+//	}
+//	
+//	
+//	
+//	//to handle specific InvalidURIException
+//	
+//		@ExceptionHandler(InvalidURIException.class)
+//		public ResponseEntity<MyErrorDetails> myIllegalHandler(InvalidURIException ie,WebRequest req) {
+//		System.out.println("inside myHandler method...");
+//		MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), req.getDescription(false));
+//		ResponseEntity<MyErrorDetails> re=new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+//		return re;
+//		}
+//
+//	
+//	
+//	
+//		@ExceptionHandler(Exception.class)
+//		public ResponseEntity<MyErrorDetails> myExceptionHandler(Exception e,WebRequest req) {
+//		MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
+//		return new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+//		}
+//
+//	
+//	@ExceptionHandler(NoHandlerFoundException.class)
+//	public ResponseEntity<MyErrorDetails> mynotFoundHandler(NoHandlerFoundException nfe,WebRequest req) {
+//	MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), nfe.getMessage(), req.getDescription(false));
+//	return new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+//	}*/
+//	
+//
+//}
