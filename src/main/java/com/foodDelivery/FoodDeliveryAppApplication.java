46d117539cc1bb9e41e30f302eca6a632b7f1c49
g-utsav <gutsav9@gmail.com>
2022-09-24 20:12:31 +0530

    Fix : DeployMent Fix

--- src/main/java/com/foodDelivery/FoodDeliveryAppApplication.java
+++ src/main/java/com/foodDelivery/FoodDeliveryAppApplication.java
@@ -2,9 +2,18 @@ package com.foodDelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class FoodDeliveryAppApplication {
+
+   	@RequestMapping("/")
+    	@ResponseBody
+    	String home() {
+      		return "Hello World!";
+   	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryAppApplication.class, args);
