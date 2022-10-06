b88c1c405b671c43369a290ebcd2cbd609d04b8d
Shubham Pawar <shubhampawar09011998@gmail.com>
2022-10-06 22:10:43 +0530

    Restaurant entity updated

--- src/main/java/com/foodDelivery/entity/Restaurant.java
+++ src/main/java/com/foodDelivery/entity/Restaurant.java
@@ -1,16 +1,21 @@
 package com.foodDelivery.entity;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 
@@ -28,87 +33,76 @@ public class Restaurant {
 	@NotNull
 	private String manager;
 	
-//	@Size(min=10,max=10)
-	private Integer contactNumber;
+	@Pattern(regexp = "[0-9]{10}",message="Mobile should be 10 digit")
+	private String contactNumber;
 	
 	//@Autowired
 	@OneToOne(cascade = CascadeType.ALL)
 	private Address address ;
 	
 	//@Autowired
-	@ManyToMany(cascade = CascadeType.ALL)
-	List<Item> itemList=new ArrayList<>();
+	@ManyToMany
+	@JoinColumn(name="item_id", nullable = false)
+	Set<Item> itemList=new HashSet<>();
 
+	public Restaurant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Restaurant(Integer resturantId, @NotNull String restaurantName, @NotNull String manager,
+			@Pattern(regexp = "[0-9]{10}", message = "Mobile should be 10 digit") String contactNumber, Address address,
+			Set<Item> itemList) {
+		super();
+		this.resturantId = resturantId;
+		this.restaurantName = restaurantName;
+		this.manager = manager;
+		this.contactNumber = contactNumber;
+		this.address = address;
+		this.itemList = itemList;
+	}
 	public Integer getResturantId() {
 		return resturantId;
 	}
-
 	public void setResturantId(Integer resturantId) {
 		this.resturantId = resturantId;
 	}
-
 	public String getRestaurantName() {
 		return restaurantName;
 	}
-
 	public void setRestaurantName(String restaurantName) {
 		this.restaurantName = restaurantName;
 	}
-
 	public String getManager() {
 		return manager;
 	}
-
 	public void setManager(String manager) {
 		this.manager = manager;
 	}
-
-	public Integer getContactNumber() {
+	public String getContactNumber() {
 		return contactNumber;
 	}
-
-	public void setContactNumber(Integer contactNumber) {
+	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
-
 	public Address getAddress() {
 		return address;
 	}
-
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-
-	public List<Item> getItemList() {
+	public Set<Item> getItemList() {
 		return itemList;
 	}
-
-	public void setItemList(List<Item> itemList) {
+	public void setItemList(Set<Item> itemList) {
 		this.itemList = itemList;
 	}
-
 	@Override
 	public String toString() {
 		return "Restaurant [resturantId=" + resturantId + ", restaurantName=" + restaurantName + ", manager=" + manager
 				+ ", contactNumber=" + contactNumber + ", address=" + address + ", itemList=" + itemList + "]";
 	}
-
-	public Restaurant(Integer resturantId, @NotNull String restaurantName, @NotNull String manager,
-			Integer contactNumber, Address address, List<Item> itemList) {
-		super();
-		this.resturantId = resturantId;
-		this.restaurantName = restaurantName;
-		this.manager = manager;
-		this.contactNumber = contactNumber;
-		this.address = address;
-		this.itemList = itemList;
-	}
-
-	public Restaurant() {
-		super();
-	}
 	
 	
-
+		
 
 }
