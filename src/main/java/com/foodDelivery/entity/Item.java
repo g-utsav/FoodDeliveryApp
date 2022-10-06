59e7c8c60ab0abe2d7eb4199ba3fedb22123ed22
Shubham Pawar <shubhampawar09011998@gmail.com>
2022-10-06 22:13:15 +0530

    Item entity updated

--- src/main/java/com/foodDelivery/entity/Item.java
+++ src/main/java/com/foodDelivery/entity/Item.java
@@ -1,87 +1,107 @@
 package com.foodDelivery.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
 public class Item {
 	
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO) 
+		private Integer itemId;
+		
+		@NotNull
+		private String itemName;
+		
+		@NotNull
+		private Double cost;
+		
+		@OneToOne(cascade = CascadeType.PERSIST)
+		private Category category;
+		
+		@ManyToMany
+		@JsonIgnore
+//		@JoinColumn(name="restaurantId")
+		private Set<Restaurant> restaurants = new HashSet<>();
+		
+
+
+	public Item(Integer itemId, @NotNull String itemName, @NotNull Double cost, Category category,
+				Set<Restaurant> restaurants) {
+			super();
+			this.itemId = itemId;
+			this.itemName = itemName;
+			this.cost = cost;
+			this.category = category;
+			this.restaurants = restaurants;
+		}
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO) 
-	private Integer itemId;
-	
-	@NotNull
-	private String itemName;
-	
-	@NotNull
-	private double cost;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	private Category category;
-	
-	@ManyToMany(cascade = CascadeType.ALL,mappedBy = "itemList")
-	List<Restaurant> restaurantList=new ArrayList<>();
+		public Item() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
 
-	public Integer getItemId() {
-		return itemId;
-	}
+		public Integer getItemId() {
+			return itemId;
+		}
 
-	public void setItemId(Integer itemId) {
-		this.itemId = itemId;
-	}
+		public void setItemId(Integer itemId) {
+			this.itemId = itemId;
+		}
 
-	public String getItemName() {
-		return itemName;
-	}
+		public String getItemName() {
+			return itemName;
+		}
 
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
+		public void setItemName(String itemName) {
+			this.itemName = itemName;
+		}
 
-	public double getCost() {
-		return cost;
-	}
+		public Double getCost() {
+			return cost;
+		}
 
-	public void setCost(double cost) {
-		this.cost = cost;
-	}
+		public void setCost(Double cost) {
+			this.cost = cost;
+		}
 
-	public Category getCategory() {
-		return category;
-	}
+		public Category getCategory() {
+			return category;
+		}
 
-	public void setCategory(Category category) {
-		this.category = category;
-	}
+		public void setCategory(Category category) {
+			this.category = category;
+		}
 
-	public Item() {
-		super();
-	}
+		@Override
+		public String toString() {
+			return "Item [itemId=" + itemId + ", itemName=" + itemName + ", cost=" + cost + ", category=" + category
+					+ ", restaurants=" + restaurants + "]";
+		}
 
-	public List<Restaurant> getRestaurantList() {
-		return restaurantList;
-	}
+		public Set<Restaurant> getRestaurants() {
+			return restaurants;
+		}
 
-	public void setRestaurantList(List<Restaurant> restaurantList) {
-		this.restaurantList = restaurantList;
-	}
+		public void setRestaurants(Set<Restaurant> restaurants) {
+			this.restaurants = restaurants;
+		}
 
-	@Override
-	public String toString() {
-		return "Item [itemId=" + itemId + ", itemName=" + itemName + ", cost=" + cost + ", category=" + category
-				+ ", restaurantList=" + restaurantList + "]";
-	}
-	
+		
+		
 }
